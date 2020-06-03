aeff3e71138702260e975c6b15430cd63ed8b286
Leonid <golani04@gmail.com>
2020-06-03 17:47:22 +0300

    registe a api blueprint

--- messaging_system/backend/api/__init__.py
+++ messaging_system/backend/api/__init__.py
@@ -3,5 +3,4 @@ from flask import Blueprint
 bp = Blueprint("api", __name__)
 
 # prevent circular imports
-from .users import users  # noqa: E402,F401
-from .messages import messages  # noqa: E402,F401
+from . import users, messages  # noqa: E402,F401
