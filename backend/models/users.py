659563970829cf69f15c5bdc34e2d711a320ef90
Leonid <golani04@gmail.com>
2020-06-16 11:42:36 +0300

    converting module to use sqlalchemy Model class + adjusting methods

--- backend/models/users.py
+++ backend/models/users.py
@@ -1,20 +1,25 @@
-from dataclasses import dataclass
 from passlib.hash import bcrypt
 
-from typing import ClassVar, Dict, List, Optional, Tuple
+from typing import Dict, List, Optional
 
+from sqlalchemy import Column, Integer, String
 from backend import db
-from .messages import Message
+
+from .mapper import recipients
 
 
-@dataclass
-class User:
-    id: int
-    name: str
-    email: str
-    password: str
-    __tablename__: ClassVar[str] = "Users"
-    __columns__: ClassVar[Tuple] = ("id", "name", "email", "password")
+class User(db.Model):
+    __tablename__ = "Users"
+
+    id = Column(Integer, primary_key=True, autoincrement=True)
+    name = Column(String(128), unique=True, nullable=True)
+    email = Column(String(128), unique=True, nullable=True)
+    password = Column(String(64), unique=True, nullable=True)
+
+    messages_sent = db.relationship("Message", back_populates="sender")
+    messages_recieved = db.relationship(
+        "Message", secondary=recipients, primaryjoin=(recipients.c.r_id == id)
+    )
 
     @staticmethod
     def generate_passw(passw: str) -> str:
@@ -25,29 +30,18 @@ class User:
         return bcrypt.verify(passw, hashed)
 
     @classmethod
-    def find_by_id(cls, id: int) -> Optional["User"]:
+    def find_by_id(cls, u_id: int) -> Optional["User"]:
         """Get user by id"""
 
-        user = db.filter_by(cls.__tablename__, {"id": id}).fetchone()
-        if user is None:
-            return None
-
-        return cls(**dict(zip(cls.__columns__, user)))
+        return cls.query.get(u_id)
 
     @classmethod
     def autenticate(cls, email: str) -> Optional["User"]:
-        user = db.filter_by(cls.__tablename__, {"email": email}).fetchone()
-        if user is None:
-            return None
+        return cls.query.filter_by(email=email).one_or_none()
 
-        return cls(**dict(zip(cls.__columns__, user)))
-
-    def get_messages(self, params: Dict = None, message_id: int = None) -> List:
+    def get_messages(self, params: Dict = None, message_id: int = None) -> Dict[str, List]:
         """Return all messages related to the user"""
-        if params is None:
-            params = {}
-
-        params = {**params, "recipient": self.id}
-        if message_id is not None:
-            params["id"] = message_id
-        return Message.filter_by(params)
+        return {
+            "recieved": self.messages_recieved,
+            "sent": self.messages_sent,
+        }
