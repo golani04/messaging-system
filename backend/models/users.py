0a24e0bd7ea54c7d628d3b7e1f08ac5e538ddfd5
Leonid <golani04@gmail.com>
2020-06-22 21:41:21 +0300

    expand user model + tests

--- backend/models/users.py
+++ backend/models/users.py
@@ -1,25 +1,30 @@
+from enum import IntEnum
 from passlib.hash import bcrypt
 
 from typing import Dict, List, Optional
 
 from sqlalchemy import Column, Integer, String
-from backend import db
+from sqlalchemy.exc import SQLAlchemyError
 
+from backend import db
+from .exceptions import SaveError, DeleteError
 from .mapper import recipients
+from .types_decorators import SQLAIntEnum
+
+
+UserRoles = IntEnum("UserRoles", "admin user")
 
 
 class User(db.Model):
     __tablename__ = "Users"
 
     id = Column(Integer, primary_key=True, autoincrement=True)
-    name = Column(String(128), unique=True, nullable=True)
-    email = Column(String(128), unique=True, nullable=True)
-    password = Column(String(64), unique=True, nullable=True)
+    name = Column(String(128), nullable=False)
+    email = Column(String(128), unique=True, nullable=False)
+    password = Column(String(64), nullable=False)
+    role = Column(SQLAIntEnum(UserRoles), index=True, default=UserRoles.user.value)
 
-    messages_sent = db.relationship("Message", back_populates="sender")
-    messages_recieved = db.relationship(
-        "Message", secondary=recipients, primaryjoin=(recipients.c.r_id == id)
-    )
+    messages_sent = db.relationship("Message", back_populates="sender", lazy="dynamic")
 
     @staticmethod
     def generate_passw(passw: str) -> str:
@@ -30,18 +35,60 @@ class User(db.Model):
         return bcrypt.verify(passw, hashed)
 
     @classmethod
+    def autenticate(cls, email: str) -> Optional["User"]:
+        return cls.query.filter_by(email=email).one_or_none()
+
+    @classmethod
+    def create(cls, name, email, password: str):
+        return cls(
+            name=name, password=cls.generate_passw(password), email=email, role=UserRoles.user
+        )
+
+    @classmethod
     def find_by_id(cls, u_id: int) -> Optional["User"]:
         """Get user by id"""
 
         return cls.query.get(u_id)
 
     @classmethod
-    def autenticate(cls, email: str) -> Optional["User"]:
-        return cls.query.filter_by(email=email).one_or_none()
+    def filter_by(cls, current_user_id: int, search_params: Dict = None):
+        if search_params is None:
+            search_params = {}
+
+        query_params = [getattr(cls, k) == v for k, v in search_params.items() if hasattr(cls, k)]
+        # do not include myself
+        query_params.append(cls.id != current_user_id)
+
+        return (
+            cls.query.join(recipients, recipients.c.r_id == current_user_id)
+            .filter(*query_params)
+            .all()
+        )
 
     def get_messages(self, params: Dict = None, message_id: int = None) -> Dict[str, List]:
         """Return all messages related to the user"""
         return {
-            "recieved": self.messages_recieved,
+            "received": self.messages_received,
             "sent": self.messages_sent,
         }
+
+    def update(self, params: Dict):
+        for k, v in params.items():
+            setattr(self, k, v)
+
+    def delete(self):
+        try:
+            db.session.delete(self)
+            db.session.commit()
+            return True
+        except SQLAlchemyError:
+            db.session.rollback()
+            raise DeleteError({"user": ("Failed to delete a user")})
+
+    def save(self):
+        try:
+            db.session.add(self)
+            db.session.commit()
+        except SQLAlchemyError:
+            db.session.rollback()
+            raise SaveError({"user": ("Failed to create/update a user")})
