d28a004ed85a72e623aa194041b24ffcb3675fc2
Leonid <golani04@gmail.com>
2020-06-30 17:44:13 +0300

    define jwt env variables in configurations

--- backend/config.py
+++ backend/config.py
@@ -1,4 +1,5 @@
 import os
+from datetime import timedelta
 from dotenv import load_dotenv
 
 
@@ -13,6 +14,14 @@ class Config:
     SQLALCHEMY_ECHO = False
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     JWT_ERROR_MESSAGE_KEY = "messages"
+    JWT_ACCESS_TOKEN_EXPIRES = timedelta(minutes=15)
+    JWT_REFRESH_TOKEN_EXPIRES = timedelta(days=1)
+    JWT_TOKEN_LOCATION = ["cookies"]
+    JWT_COOKIE_SECURE = True
+    JWT_ACCESS_COOKIE_PATH = "/api"
+    JWT_REFRESH_COOKIE_PATH = "/auth/refresh"
+    JWT_COOKIE_CSRF_PROTECT = True
+    JWT_CSRF_IN_COOKIES = False
 
 
 class DevConfig(Config):
