7de9e30b359924c5308df4ef5e250bf626e270c7
Leonid <golani04@gmail.com>
2020-07-16 23:31:37 +0300

    wip: home page

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,43 +1,17 @@
-import React, { useState } from 'react';
-import axios from 'axios';
-import './App.css';
+import React from 'react';
+import Container from 'react-bootstrap/Container';
+import SignInAndSignUpPage from './pages/authentication/auth.pages';
 
 function App() {
-  const [email, setUsername] = useState('');
-  const [password, setPassword] = useState('');
-  const [tokens, setTokens] = useState('');
-
-
-  const handleSubmit = async e => {
-    e.preventDefault();
-
-    const user = { email, password };
-    const response = await axios.post('/auth/login', user);
-
-    // get
-    setTokens(response.data);
-  };
-
-  if (tokens) {
-    return <div>User is logged in.</div>;
-  }
-
-
   return (
-    <div className="App">
-      <form onSubmit={handleSubmit}>
-        <div>
-          <label htmlFor="username">Email: </label>
-          <input type="email" value={email} placeholder="enter an email" onChange={({ target }) => setUsername(target.value)} />
-        </div>
-        <div>
-          <label htmlFor="username">Password: </label>
-          <input type="password" value={password} placeholder="enter a password" onChange={({ target }) => setPassword(target.value)} />
-        </div>
-        <button type="submit">Login</button>
-      </form>
-    </div>
+    <Container>
+      {/* Header */}
+      <SignInAndSignUpPage />
+      {/* Footer */}
+    </Container>
   );
+
+  // add router to handle navigation between logged in user and anonymous
 }
 
 export default App;
